86ba69d3b7af3d6ebcd39c6d60986fcabff2b541
Kyoungeun Jo <kavvy99@naver.com>
2024-05-21 18:27:47 +0900

    1546: 평균

---
+++ 1546.py
@@ -0,0 +1,13 @@
+# 시험 본 과목의 개수 N (N <= 1000)
+N = int(input())
+# 세준이의 현재 성적
+scores = list(map(int, input().split()))
+
+max_score = max(scores)
+
+for i in range(N):
+    scores[i] = scores[i] / max_score * 100
+    
+print(sum(scores)/N)
+
+
