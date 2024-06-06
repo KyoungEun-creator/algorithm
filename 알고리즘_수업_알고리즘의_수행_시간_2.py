08dad92ada7dd25d8911572bda8e9363b2e8870e
Kyoungeun Jo <kavvy99@naver.com>
2024-06-06 11:04:27 +0900

    24263: 알고리즘 수업2

---
+++ 알고리즘_수업_알고리즘의_수행_시간_2.py
@@ -0,0 +1,17 @@
+# 24263: 알고리즘 수업 - 알고리즘의 수행 시간 2
+
+# 아래 알고리즘의 수행 횟수 출력
+# 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력
+'''
+def MenOfPassion(A, n):
+    sum = 0
+    for i in range(1, n):
+        sum += A[i]
+    return sum
+'''
+
+n = int(input())
+
+print(n)
+print(1)
+
