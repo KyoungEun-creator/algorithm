5e10a340e996bbea37c185cff24431802d307de8
Kyoungeun Jo <kavvy99@naver.com>
2024-06-14 23:58:11 +0900

    14425: 문자열 집합

---
+++ 문자열_집합.py
@@ -0,0 +1,29 @@
+# 14425: 문자열 집합
+
+'''
+첫째 줄에 문자열의 개수 N과 M (1 ≤ N ≤ 10,000, 1 ≤ M ≤ 10,000)이 주어진다. 
+다음 N개의 줄에는 집합 S에 포함되어 있는 문자열들이 주어진다.
+다음 M개의 줄에는 검사해야 하는 문자열들이 주어진다.
+입력으로 주어지는 문자열은 알파벳 소문자로만 이루어져 있으며, 길이는 500을 넘지 않는다. 
+집합 S에 같은 문자열이 여러 번 주어지는 경우는 없다.
+
+첫째 줄에 M개의 문자열 중에 총 몇 개가 집합 S에 포함되어 있는지 출력한다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+N, M = list(map(int, input().split()))
+S = []
+cnt = 0
+
+
+for _ in range(N):
+    word = input()
+    S.append(word)
+for _ in range(M):
+    check = input()
+    if check in S:
+        cnt += 1
+
+print(cnt)
